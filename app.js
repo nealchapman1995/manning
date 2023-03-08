13e67a224eb63482d13bf32fd07be9c1f7cd94c1
nealchapman1995 <neal.chapman1@gmail.com>
2023-03-07 18:53:13 -0800

    Adding CRUD to Squads

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const methodOverride = require('method-override');
 var db = require('./db-connector')
 
 CreateSoldierTable = "CREATE TABLE `Soldiers` (`soldier_id` int(11) NOT NULL AUTO_INCREMENT,`soldier_name` varchar(45) NOT NULL,`rank` varchar(45) NOT NULL,`address` varchar(45) DEFAULT NULL,`soldier_status` varchar(45) DEFAULT NULL,`Squads_squad_id` int(11) NOT NULL,`Weapons_weapon_id` int(11) NOT NULL, PRIMARY KEY (soldier_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;"
+CreateWeaponTabler = "CREATE TABLE `Weapons` (`weapon_id` int(11) NOT NULL, `weapon_model` varchar(45) NOT NULL, `weapon_status` varchar(45) DEFAULT NULL, `Squads_squad_id` int(11) DEFAULT NULL PRIMARY KEY (weapon_id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;"
 SelectSoldiers = "SELECT * FROM Soldiers;";
 SelectWeapons = "SELECT * FROM Weapons;";
+SelectSquad = "SELECT * FROM Squads;";
 
 
 app.set('view engine', 'ejs');
@@ -19,9 +21,12 @@ app.use(methodOverride('_method'));
 
 let weapons = ""
 let soldiers = ""
+let squads = ""
 
 db.pool.query(CreateSoldierTable, function(err, results, fields){
-})
+});
+
+
 
 
 /*
@@ -33,38 +38,59 @@ app.get('/', function(req, res)
             weapons = results
         })
 
+        db.pool.query(SelectSquad, function(err, results, fields){
+            squads = results
+        })
+
         db.pool.query(SelectSoldiers, function(err, results, fields){
-            soldiers = results  
-            res.render('soldiers', {soldiers})  
+            soldiers = results 
+            res.render('soldiers', {soldiers, weapons, squads})  
         })
          
     });
 
 app.post('/new_soldier', async(req, res) => {
-    let insert = `INSERT INTO Soldiers (soldier_name, rank, address, soldier_status, Squads_squad_id, Weapons_weapon_id) VALUES('${req.body.soldier.name}', '${req.body.soldier.rank}', '${req.body.soldier.address}', '${req.body.soldier.status}', ${req.body.soldier.squadID}, ${req.body.soldier.weaponID})`;
-    db.pool.query(insert, function(err, results, fields) {
-        console.log
+    let soldier_insert = `INSERT INTO Soldiers (soldier_name, rank, address, soldier_status, Squads_squad_id, Weapons_weapon_id) VALUES('${req.body.soldier.name}', '${req.body.soldier.rank}', '${req.body.soldier.address}', '${req.body.soldier.status}', ${req.body.soldier.squadID}, ${req.body.soldier.weaponID})`;
+    db.pool.query(soldier_insert, function(err, results, fields) {
         res.redirect('/')
     })
 })
 
 app.delete('/delete_soldier/:id', async(req, res) => {
-    let sql_delete = `DELETE FROM Soldiers WHERE soldier_id = ${req.params.id}`;
-    db.pool.query(sql_delete, function(err, results, fields) {
+    let soldier_delete = `DELETE FROM Soldiers WHERE soldier_id = ${req.params.id}`;
+    db.pool.query(soldier_delete, function(err, results, fields) {
         res.redirect('/')
     })
 })
 
 app.put('/edit_soldier', async(req, res) => {
-    let sql_edit = `UPDATE Soldiers SET soldier_name = '${req.body.soldier.name}', rank = '${req.body.soldier.rank}', address = '${req.body.soldier.address}', soldier_status = '${req.body.soldier.status}', Squads_squad_id = ${req.body.soldier.squadID}, Weapons_weapon_id = ${req.body.soldier.weaponID} WHERE soldier_id = ${req.body.soldier.id}`;
-    db.pool.query(sql_edit, function(err, results, fields) {
-        console.log(err)
+    let soldier_edit = `UPDATE Soldiers SET soldier_name = '${req.body.soldier.name}', rank = '${req.body.soldier.rank}', address = '${req.body.soldier.address}', soldier_status = '${req.body.soldier.status}', Squads_squad_id = ${req.body.soldier.squadID}, Weapons_weapon_id = ${req.body.soldier.weaponID} WHERE soldier_id = ${req.body.soldier.id}`;
+    db.pool.query(soldier_edit, function(err, results, fields) {
         res.redirect("/")
     })
 })
 
+app.post('/squads/new', (req, res) => {
+    let squad_insert = `INSERT INTO Squads (squad_leader, squad_name) VALUES ('${req.body.squad.leader}', '${req.body.squad.name}');`
+    db.pool.query(squad_insert, function(err, results, fields){
+        console.log(err)
+        res.redirect('/squads')
+    })
+})
+
+app.delete('/squads/delete/:id', async(req, res) => {
+    let squad_delete = `DELETE FROM Squads WHERE squad_id = ${req.params.id}`;
+    db.pool.query(squad_delete, function(err, results, fields) {
+        console.log(err)
+        res.redirect('/squads')
+    })
+})
+
 app.get('/squads', function(req, res){
-    res.render('squads')
+    db.pool.query(SelectSquad, function(err, results, fields){
+        squads = results
+        res.render('squads', {squads})
+    })
 });
 
 app.get('/weapons', function(req, res){
--- views/README.md
+++
@@ -1,2 +0,0 @@
-# manning
-# manning
